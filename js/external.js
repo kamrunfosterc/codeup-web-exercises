efe8e46a606d30851867efd9afb990c443e5578c
Kamrun Foster <kamrun.foster4@gmail.com>
2021-05-26 16:42:36 -0500

    working on hw exercises

--- js/external.js
+++ js/external.js
@@ -1,1 +1,41 @@
+// exercise 1
 console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+
+// exercise 2
+var fav_color = prompt("What is your favorite color?");
+alert("Great, " + fav_color + " that is my favorite color too!");
+
+// exercise 3
+
+// part A movie rental
+var movie_per_day = 3.00;
+var lil_mermaid = 3;// rented for 3 days
+var brother_bear = 5;// rented for 5 days
+var hercules = 1;// rented for a day
+
+var total_rental_cost = (lil_mermaid+brother_bear+hercules) * movie_per_day;
+console.log("Your total purchase cost is $" + total_rental_cost);
+
+//part B hourly pay
+var google_hrly =400;// hourly pay rate
+var amazon_hrly =380;// hourly pay rate
+var facebook_hrly =350;// hourly pay rate
+
+hrs_at_google = prompt("How many hours did we work at Google? ")
+hrs_at_amazon = prompt("How many hours did we work at Amazon? ")
+hrs_at_facebook = prompt("How many hours did we work at Facebook? ")
+
+alert("You're working with google, amazon & facebook for " + hours + " this week!")
+var total_hrs_pay = ((google_hrly*6)+(amazon_hrly*4)+(facebook_hrly*10));
+console.log(" You are being paid $" + total_hrs_pay + " dollars this week!");
+
+// A student can be enrolled in a class only if the class is not full
+// and the class schedule does not conflict with her current schedule.
+
+var student = "";
+
+// A product offer can be applied only if a person buys more than 2 items,
+// and the offer has not expired. Premium members do not need to buy a
+// specific amount of products.
+
