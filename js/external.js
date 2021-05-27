6f8044e7e5003370eff0ce8452dce9be3e972bb1
Kamrun Foster <kamrun.foster4@gmail.com>
2021-05-27 14:17:10 -0500

    went over exercises in class

--- js/external.js
+++ js/external.js
@@ -1,12 +1,27 @@
-// exercise 1
+"use strict"
+//console.log("hello world");
+//console.log("Hello from external JavaScript");
+//alert("Welcome to my website!");
+//var favorite_color = prompt("What is your favorite color?");
+//alert("Great, " + favorite_color + " is my favorite color too!");
+//alert("Hello World!");
+//var confirmed = confirm('Are you sure you want to do XYZ?');
+//console.log(confirmed); // will be either true or false
+
+//var userInput = prompt('Please type something:');
+//console.log('The user entered: ' + userInput);
+
+console.log("hello world");
+
+// exercise 1 ******** ******* *******
 console.log("Hello from external JavaScript");
 alert('Welcome to my Website!');
 
-// exercise 2
+// exercise 2 ******** ******* *******
 var fav_color = prompt("What is your favorite color?");
 alert("Great, " + fav_color + " is my favorite color too!");
 
-// exercise 3
+// exercise 3 ******** ******* *******
 
 // part A movie rental
 alert("Let's check out your three movies.");
@@ -19,37 +34,37 @@ var days_hercules = 1;// rented for a day
 
 var total_rental_cost = (days_lil_mermaid + days_brother_bear + days_hercules) * movie_per_day;
 console.log(total_rental_cost.toFixed(2));
-alert("Your total rental cost is $" + total_rental_cost.toFixed(2));
+alert("Your total rental cost is $" + total_rental_cost.toFixed(2) + " for Little Mermaid, Hercules & Brother Bear");
 
 // *********** ************ *********** *************
 
-//part B hourly pay
+//part B hourly pay ******** ******* *******
 var google_rate =400;// hourly pay rate
 var amazon_rate =380;// hourly pay rate
 var facebook_rate =350;// hourly pay rate
 
-hrs_at_google = prompt("How many hours did we work at Google? ");//enter 6 hours
-hrs_at_amazon = prompt("How many hours did we work at Amazon? ");// enter 4 hours
-hrs_at_facebook = prompt("How many hours did we work at Facebook? ");// enter 10 hours
+var hrs_at_google = prompt("How many hours did we work at Google? ");//enter 6 hours
+var hrs_at_amazon = prompt("How many hours did we work at Amazon? ");// enter 4 hours
+var hrs_at_facebook = prompt("How many hours did we work at Facebook? ");// enter 10 hours
 
 var amazon_hours = Number(hrs_at_amazon);
 var facebook_hours = Number(hrs_at_facebook);
 var google_hours = Number(hrs_at_google);
 
-console.log(amazon_hours, facebook_hours, google_hours);
+console.log(amazon_hours + "hrs", facebook_hours + "hrs", google_hours + "hrs");
 
 
 var total_hours = parseInt(amazon_hours + facebook_hours + google_hours);
 
 var total_pay = (google_rate*google_hours)+(amazon_rate*amazon_hours)+(facebook_rate*facebook_hours);
-
+// var totaly_pay = (googlhr*googlrate + amazonhr*amazonrate + fbhr*fbrate);
 alert("You worked a total of "+ total_hours + " hours this week!");
 alert( "Your compensation will be $"+ total_pay + " dollars. Thank you for your efforts!")
 console.log(total_hours+ "hours", "$"+total_pay);
 
 
 
-//Student exercise for getting into class
+//Student exercise for getting into class ******** ******* *******
 
 alert("How many students are there and does our schedule conflict with student?")
 function course(student_numb, schedule_conflict) {
@@ -59,9 +74,15 @@ function course(student_numb, schedule_conflict) {
         return "Sorry, please apply for a different semester."
     }
 }
+/* alternatively
+* alert if class is full, alert if theres a conflict converted to boolean
+* to answer above question
+* var full = confirm("is the class full?);
+* var conflict = confirm("is there a conflict with your schedule")
+* alert("you can enroll in class" + (!full && !conflict)); */
 
 
-//Promotional offer Exercise
+//Promotional offer Exercise ******** ******* *******
 function product(item, premium_member, expiration) {
     if (item > 2 && expiration == false) {
         return "Congratulations on our product offer!"
