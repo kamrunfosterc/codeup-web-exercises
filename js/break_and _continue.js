6d76ed7aacf7d77797c4d61e5b06f0eea25783f8
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-04 17:05:26 -0500

    break and continue exercise

--- js/break_and _continue.js
+++ js/break_and _continue.js
@@ -13,6 +13,7 @@
         //     }
         // }
 
+
 // MY CODE
     // for (let oddNumb = ""; oddNumb <= 50; oddNumb ++){
     //     oddNumb = parseInt(prompt("Pick an odd number between 1 & 50 "));
@@ -29,4 +30,58 @@
     //     }
     // }
 
+// DO WHILE loop exercise
+//var allCones = Mc
 
+
+// kathleen version
+var allConesAvail = Math.floor(Math.random() * (100-50)) + 50;
+console.log("The number of cones I can seel" + allConesavail);
+
+let totalgone = 0; //starts total sold at 0
+do {
+    let purchased = Math.floor(Math.random() * (5-1)) + 1;
+
+    if((totalgone + purchased) > totalgone){
+        console.log("Sorry, Can't do sell "+ purchased);
+        continue;
+    }
+    console.log("My customer bought "+ purchased);
+    totalgone = totalgone + purchased;
+    console.log("I've sold this many " + totalgone)
+
+
+} while (totalgone < allConesAvail);
+
+
+console.log("we don't have enough cones");// if remaining amount is larger than what we have
+console.log("We can't sell you " + + " we only have " + + " left, Sorry.");// can't sell you 6 i only have 3
+console.log("YAY! is sold them all! No more cones "); // we are done
+
+do { //writing games, checking out shopping cart, input validation
+    // do stuff
+    alert('Busy, doing stuff!')
+    notDone = confirm("Do again? ");
+} while(notDone);
+
+
+//WHAT WE DID, NEED TO PRACTICE THIS ONE!!! AND UNDERSTAND
+
+// // Determine the total ice cream to be sold
+// let totalCones = Math.ceil(Math.random() * (100 - 50) + 50);
+// console.log("Total to be sold: " + totalCones);
+//
+// // running total
+// let total = 0;
+//
+// do {
+//     // How many cones were bought
+//     let bought = Math.ceil(Math.random() * (5 - 1) + 1);
+//
+//     if (total + bought > totalCones) {
+//         continue;
+//     }
+//     total = total + bought;
+//     console.log("Customer bought: " + bought);
+//     console.log("Total Sold: " + total);
+// } while (total < totalCones);
