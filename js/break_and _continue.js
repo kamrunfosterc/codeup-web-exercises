3ef382648041f3a272a4101b6105e2708ea959d3
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-04 16:03:08 -0500

    odd number

--- js/break_and _continue.js
+++ js/break_and _continue.js
@@ -1,9 +1,32 @@
 "use strict";
 
 // BREAK & CONTINUE EXERCISES
-for (var index = prompt("Pick a number between 1 & 50 "); index <= 50; index ++){
-    if (index % 2 == 0){
-        continue;
-    }
-    console.log("This is an odd Number, " + index );
-}
+
+// Kathleen and Brittney's
+        // var oddNumber;
+        //
+        // console.log("Number to skip is: " + oddNumber);
+        // while(true){
+        //     oddNumber = parseInt(prompt("Pick an odd number between 1 & 50 "));
+        //     if(oddNumber % 2 !== 0){
+        //         break;
+        //     }
+        // }
+
+// MY CODE
+    // for (let oddNumb = ""; oddNumb <= 50; oddNumb ++){
+    //     oddNumb = parseInt(prompt("Pick an odd number between 1 & 50 "));
+    //     if (oddNumb % 2 === 0){
+    //         alert("That's not an odd number!");
+    //         continue;// should skip pass any even numbers after number entry
+    //     }
+    //     console.log("This is an odd Number, " + oddNumb );
+    //
+    //     if (oddNumb % 2 !== 0){
+    //         alert(oddNumb + " is a nice choice!");
+    //         break;
+    //         //oddNumb = confirm("Please pick a number between 1 & 50 ");
+    //     }
+    // }
+
+
