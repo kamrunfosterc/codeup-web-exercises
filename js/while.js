c6b0e798595621b56bb929082f4a3e511f919257
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-04 14:11:35 -0500

    WHILE loops notes

---
+++ js/while.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('*************** WHILE loops ***************')
+
+var index = 0; //have to initize each condition before entering loop
+while (index < 8){// ran when we aren't sure how many times we need to run something
+    // do something
+    console.log(index);
+    index++;
+}
