0c42fd64c503253d8745ef7c3e9e74fd32e1efac
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-08 11:03:41 -0500

    object lecture update

--- js/objects_lecture.js
+++ js/objects_lecture.js
@@ -8,5 +8,47 @@
      year: 2012,
      make: 'Honda',
      model:'Civic',
+     info: infoFunction;
  };
  console.log(car);
+ document.write(car.year +' '+ car.make +' '+ car.model);// displays information on webpage
+
+// can add values to objects ...
+// i.e. car.numberOfCylinders = 4: adds this key value pair
+// i.e. car['numberOfSeats'] = 5; this is BEST WAY to create DYNAMICALLY properties
+
+console.log('\n Create a Person')
+var person = {};
+person = {
+    name: "Rango",
+    heightInInches: 68,
+    age: 45,
+}
+car.owner = person; //NESTED object
+
+var child = {};
+child = {
+    name: "Mylz",
+    heightInInches: 21,
+    age: 2,
+}
+
+person.son = child;
+car['owner'] = person;
+console.log(car);
+console.log(" \n *** Creating a Method ***");
+
+car.info = function (){// created function that allows us to call
+    return car.year +', '+ car.make +','+ car.model;
+}
+
+car.info1 = function infoFunction(name){// created function that allows us to call >>> KEEP FUNCTIONS SIMPLE IN OBJECTS!!
+    if (name !== undefined){
+        return "Hi, my name is  " + name + " and I own a " + " " + car.year +', '+ car.make +','+ car.model;
+    }
+    return this.year +', '+ this.make +','+ this.model; // replacing car == 'this'
+}
+console.log(car.info1);
+// 'this' keyword comes into play, where info needs to come from the current property we are on
+car.info1 = infoFunction;
+//info: infoFunction;
