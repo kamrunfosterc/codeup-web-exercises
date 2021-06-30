8c477f54789bc0b4f30115fa4d1c7b50c1de956a
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-30 16:43:20 -0500

    updating and working on practice content 6/30/21

--- js/Returnt_to_JavaScript.js
+++ js/Returnt_to_JavaScript.js
@@ -75,12 +75,78 @@ console.log("firstAndLastName <blank> and smith: " + firstAndLastName(" ","smith
 // if name is longer than 10 chars, return first 7 characters and...
 function shortenedFullName(firstName, lastName){
     var shortName = firstAndLastName(firstName, lastName);
+
+    // if shortName has more than 10 characters, then shorten to 7
+    if (shortName.length > 10){
+        //w3schools.com/js/js_string_methods.asp
+        var shorterName = shorterName.substr(0,7); // starts at character zero and only counts to 7
+        return shorterName + "...";
+
+    }
     return shortName;
 }
 
-console.log(shortenedFullName("larry","hoover"))
+console.log(shortenedFullName("larry","hoover"));
+console.log(shortenedFullName("Princess","Dianna"));
+console.log(shortenedFullName("Mendikan","Bias"));
 
 
+
+
+//
+// ​
+// function foo1() {
+//     console.log("inside foo1");
+// }
+// ​
+// function foo2() {
+//     return "hello worldXXX";
+// }
+// ​
+// function multiplyTwoNumbers(myParameter1, myParameter2) {
+//     return myParameter1 * myParameter2;
+// }
+// ​
+// // returns concatenation of first and last name
+// // first and last name cannot be blank
+// // returns an empty string if either first or last name are blank
+// function firstAndLastName(firstName, lastName) {
+//     if(firstName.length < 1) {
+//         console.log("ERROR: first name cannot be blank!");
+//         return "";
+//     }
+//     if(lastName.length < 1) {
+//         console.log("ERROR: last name cannot be blank!");
+//         return "";
+//     }
+// ​
+//     return firstName + " " + lastName;
+// }
+// ​
+// // return shortened version of full name given first and last name
+// // i.e., at most 10 characters of full name
+// // if name is longer than 10 chars, return first 7 characters and ...
+// function shortenFullName(firstName, lastName) {
+//     var shortName = firstAndLastName(firstName, lastName);
+// ​
+//     // if shortName has more than 10 characters, then shorten it with ...
+//     if(shortName.length > 10) {
+//         return shortName.substr(0, 7) + "...";
+//     }
+//     return shortName;
+// }
+// ​
+// // foo1();
+// // console.log(foo2());
+// // console.log("call to multiply 2 numbers is : " + multiplyTwoNumbers(4, 5));
+// // console.log("call to multiply 2 numbers is : " + multiplyTwoNumbers(10, 20));
+// ​
+// // console.log("firstAndLastName bob and smith: " + firstAndLastName("bob", "smith"));
+// // console.log("firstAndLastName <blank> and smith: " + firstAndLastName("", "smith"));
+// // console.log("firstAndLastName bob and <blank>: " + firstAndLastName("bob", ""));
+// ​
+// // console.log(shortenFullName("bob", "smith"));
+// console.log(shortenFullName("william", "smith")); // expected william...
 
 
 
