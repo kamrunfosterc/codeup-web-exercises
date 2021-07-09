94eeca53ef3669555d1494f2e20f74d29a302b74
Kamrun Foster <kamrun.foster4@gmail.com>
2021-07-09 00:03:06 -0500

    updated assessment review

--- js/ObjectsAndArrayPractice.js
+++ js/ObjectsAndArrayPractice.js
@@ -4,178 +4,287 @@
 //
 // 1. Create a function called `removeDuplicates` that accepts an array of strings and returns the array with all duplicate elements filtered out.
 //     ### Examples
-    ```js
-removeDuplicates(["bob","squid","samantha","bob"]);
-// Should return the following
-["bob","squid","samantha"]
-
-removeDuplicates(["tilda","tilda"]);
-// Should return the following
-["tilda"]
-
-removeDuplicates(["2016-02-13","1994-12-21","2016-02-13","2016-02-13","2005-11-29"]);
-// Should return the following
-["1994-12-21","2016-02-13","2005-11-29"]
-```
+//     ```js
+// removeDuplicates(["bob","squid","samantha","bob"]);
+// // Should return the following
+// ["bob","squid","samantha"]
+//
+// removeDuplicates(["tilda","tilda"]);
+// // Should return the following
+// ["tilda"]
+//
+// removeDuplicates(["2016-02-13","1994-12-21","2016-02-13","2016-02-13","2005-11-29"]);
+// // Should return the following
+// ["1994-12-21","2016-02-13","2005-11-29"]
+// ```
 
 // 2. Create a function called `max` that accepts an array of numbers and returns the largest number from that array.
 //     ### Examples
-    ```js
-max([4,34,193,2,345,46,0]);
-// Should return the following 345
+//     ```js
+// max([4,34,193,2,345,46,0]);
+// // Should return the following 345
+//
+// max([-34,83,21,38,-2039,3.2]);
+// // Should return the following 83
+//
+// max([54.5,2,34,-93]);
+// // Should return the following 54.5
+// ```
+// ******** ******** ******** ******** ******** ********
+//         function max(arrayOfNumbers){
+//           // return Math.max(...arrayOfNumbers); // (OPTION 1) should return 54.5
+//         var largestNumber = [];// setting up empty array bc we are dealing w/ an array of numbers
+//             for (var i = 0; i < arrayOfNumbers.length; i++) {
+//                 if (largestNumber === undefined){
+//                     // since it's an empty array we assume 1st loop will give nothing so we assign a value for it being undefined
+//                     largestNumber = arrayOfNumbers[0];// QUESTION does the [0] give a value of 0 or place in array of 0
+//                 }
+//                 if (arrayOfNumbers[i] > largestNumber){
+//                     console.log("new array is larger than previous");
+//                     largestNumber = arrayOfNumbers[i];// assigns new largest number if it's larger than the previous iteration
+//                 }
+//             }
+//             return largestNumber;
+//         } console.log(max([54.5, 2, 34, -93]));// (OPTION 2) should return 54.5
+//
+//         function max(arrOfNumbers){
+//             var largestNumber = [];
+//             arrOfNumbers.forEach(function (singleNumber){
+//                 if (largestNumber === undefined){
+//                     largestNumber = singleNumber;
+//                 }
+//                 if (singleNumber > largestNumber){
+//                     largestNumber = singleNumber;
+//                 }
+//             })
+//             return largestNumber;
+//         } // (OPTION 3) should return 54.5
+// ******** ******** ******** ******** ******** ********
 
-max([-34,83,21,38,-2039,3.2]);
-// Should return the following 83
-
-max([54.5,2,34,-93]);
-// Should return the following 54.5
-```
 
 // 3. Create a function called `mostOccuringNumber` that accepts an array of numbers and returns the number that occurs the most within that array.
 //     ### Examples
-    ```js
-mostOccuringNumber([8,93,28,8,27,-62,32,8,-62]);
-// Should return the following 8
+//     ```js
+function mostOccuringNumber(arrayOfNumbers){
 
-mostOccuringNumber([8,93,-62.0,28,27,-62,32,8,-62]);
-// Should return the following -62
-
-mostOccuringNumber([73.5,8,0,-3,3.50,8.25,3.50]);
-// Should return the following 3.5
-```
+}
+// mostOccuringNumber([8,93,28,8,27,-62,32,8,-62]);
+// // Should return the following 8
+//
+// mostOccuringNumber([8,93,-62.0,28,27,-62,32,8,-62]);
+// // Should return the following -62
+//
+// mostOccuringNumber([73.5,8,0,-3,3.50,8.25,3.50]);
+// // Should return the following 3.5
+// ```
 
 // 4. Create a function called `whoIsPassing` that accepts an array of objects, where each object is a student's name and class average (from 0 to 100). Your function will return an array of objects where each object is a student's name and whether he/she is passing. Note that a class average of 60 and higher is passing.
 //     ### Examples
-    ```js
-var students = [
-    {
-        name: "Phill Rundy",
-        classAverage: 53
-    },
-    {
-        name: "Samuel Moore",
-        classAverage: 71
-    },
-    {
-        name: "Mark Tendly",
-        classAverage: 84
-    },
-    {
-        name: "Dough Mosh",
-        classAverage: 87
-    },
-    {
-        name: "Bran Thomb",
-        classAverage: 93
-    },
-    {
-        name: "Mario Yushi",
-        classAverage: 82
-    },
-    {
-        name: "Nathan Skywalker",
-        classAverage: 52
-    }
-]
-
-whoIsPassing(students);
-// Should return the following
-[
-    {
-        name: "Phill Rundy",
-        passing: false
-    },
-    {
-        name: "Samuel Moore",
-        passing: true
-    },
-    {
-        name: "Mark Tendly",
-        passing: true
-    },
-    {
-        name: "Dough Mosh",
-        passing: true
-    },
-    {
-        name: "Bran Thomb",
-        passing: true
-    },
-    {
-        name: "Mario Yushi",
-        passing: true
-    },
-    {
-        name: "Nathan Skywalker",
-        passing: false
-    }
-]
-```
+//     ```js
+// ******** ******** ******** ******** ******** ********
+// function whoIsPassing(arrayOfStudents){
+//             var passingResult = [];
+//     for (let i = 0; i < arrayOfStudents.length; i++) {
+//         if (arrayOfStudents[i].classAverage >= 60){
+//             //meaning this student avg grade is passing
+//             passingResult.push({
+//                     name: arrayOfStudents[i].name,
+//                     passing: true
+//                 })
+//         } else {
+//             passingResult.push({
+//                 name: arrayOfStudents[i].name,
+//                 passing: false
+//             })
+//         }
+//
+//     } return passingResult;
+// } console.log(whoIsPassing(students));// (OPTION 1)
+//
+// function whoIsPassing(arrayOfStudents){
+//     var passingResults = [];
+//     for (let i = 0; i < arrayOfStudents.length; i++) {
+//         var currentStudentName = arrayOfStudents[i].name;
+//         var isCurrentStudentPassing = (arrayOfStudents[i].classAverage >= 60);
+//         passingResults.push({
+//             name: currentStudentName,
+//             passing: isCurrentStudentPassing,
+//         })
+//     }
+//
+//     return  passingResults;// we want to return what we pass
+// }// (OPTION 2)
+// ******** ******** ******** ******** ******** ********
+// var students = [
+//     {
+//         name: "Phill Rundy",
+//         classAverage: 53
+//     },
+//     {
+//         name: "Samuel Moore",
+//         classAverage: 71
+//     },
+//     {
+//         name: "Mark Tendly",
+//         classAverage: 84
+//     },
+//     {
+//         name: "Dough Mosh",
+//         classAverage: 87
+//     },
+//     {
+//         name: "Bran Thomb",
+//         classAverage: 93
+//     },
+//     {
+//         name: "Mario Yushi",
+//         classAverage: 82
+//     },
+//     {
+//         name: "Nathan Skywalker",
+//         classAverage: 52
+//     }
+// ]
+//
+// whoIsPassing(students);
+// // Should return the following
+// [
+//     {
+//         name: "Phill Rundy",
+//         passing: false
+//     },
+//     {
+//         name: "Samuel Moore",
+//         passing: true
+//     },
+//     {
+//         name: "Mark Tendly",
+//         passing: true
+//     },
+//     {
+//         name: "Dough Mosh",
+//         passing: true
+//     },
+//     {
+//         name: "Bran Thomb",
+//         passing: true
+//     },
+//     {
+//         name: "Mario Yushi",
+//         passing: true
+//     },
+//     {
+//         name: "Nathan Skywalker",
+//         passing: false
+//     }
+// ]
+// ```
 
 // 5. Create a function called `dateStringToObject` that accepts a date in the following string format "YYYY-MM-DD" and returns an object that contains month, day, and year properties.
 //     ### Examples
-    ```js
-dateStringToObject("2016-2-13");
-// Should return the following {month: 2, day: 13, year: 2016}
+//     ```js
 
-dateStringToObject("2005-11-29");
-// Should return the following {month: 11, day: 29, year: 2005}
+// ******** ******** ******** ******** ******** ********
+// function dateStringToObject(date){
+//             var splitString = date.split("-");// gonna set up our YYYY-MM-DD format
+//             var dateObject = {}; // object variable we will push results to
+//     // date should have a year(1), month(2), day(3) array input
+//     dateObject.year = splitString[0];
+//     dateObject.month = splitString[1];
+//     dateObject.day = splitString[2];
+//     return dateObject;
+// } //(OPTION 1)
+// function dateStringToObject(date){
+//     var splitDate = date.split("-");
+//     return {year: splitDate[0], month: splitDate[1], day: splitDate[3]};
+// } //(OPTION 2)
+// ******** ******** ******** ******** ******** ********
 
-dateStringToObject("1994-12-21");
-// Should return the following {month: 12, day: 21, year: 1994}
-```
+// dateStringToObject("2016-2-13");
+// // Should return the following {month: 2, day: 13, year: 2016}
+//
+// dateStringToObject("2005-11-29");
+// // Should return the following {month: 11, day: 29, year: 2005}
+//
+// dateStringToObject("1994-12-21");
+// // Should return the following {month: 12, day: 21, year: 1994}
+// ```
 
 // 6. Create a function called `reverseString` that that accepts a string and returns a string with the characters in reverse order.
 //     ### Examples
-    ```js
-reverseString("squid");
-// Should return the following "diuqs"
+//     ```js
+function reverseString(str){
+    var splitReverseJoin = str.split().reverse().join("");//taking stringk splittingk reverse order, and rejoin together to reverse string
+    return splitReverseJoin;
+} console.log(reverseString("Amanda"));// (OPTION 1)
 
-reverseString("tilda");
-// Should return the following "adlit"
+function reverseString(str){
+    var splitArray = str.split("");
+    var reverseArray= str.reverse();
+    var joinArray = str.join("");
 
-reverseString("2016-02-13");
-// Should return the following "31-21-6102"
-```
+    return joinString;
+}
+// reverseString("squid");
+// // Should return the following "diuqs"
+//
+// reverseString("tilda");
+// // Should return the following "adlit"
+//
+// reverseString("2016-02-13");
+// // Should return the following "31-21-6102"
+// ```
 
 // 7. Create a function called `numberInfo` that accepts a number and returns an object with the properties isPositive, isEven, isZero. The properties should either be true or false based on if the parameter meets the criteria.
 //     ### Examples
-    ```js
-numberInfo(-1);
-// Should return the following
-{
-    isPositive: false,
-    isEven: false,    
-    isZero: false
+//     ```js
+function numberInfo(numb){
+
+
+            return {
+                isPositive: (numb > 0),// looks like should return boolean value/ like putting an if statement in here
+                isEven: (numb%2 === 0),
+                isZero: (numb === 0),
+
+            }
 }
+// numberInfo(-1);
+// // Should return the following
+// {
+//     isPositive: false,
+//     isEven: false,
+//     isZero: false
+// }
+//
+// numberInfo(6);
+// // Should return the following
+// {
+//     isPositive: true,
+//     isEven: true,
+//     isZero: false
+// }
+//
+// numberInfo(0);
+// // Should return the following
+// {
+//     isPositive: false,
+//     isEven: true,
+//     isZero: true
+// }
+// ```
 
-numberInfo(6);
-// Should return the following
-{
-    isPositive: true, 
-    isEven: true,
-    isZero: false
-}
-
-numberInfo(0);
-// Should return the following
-{
-    isPositive: false,
-    isEven: true,
-    isZero: true
-}
-```
 
 
 
 
-
-
-// 1st time for first problem
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXX XXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// 2. 1st time for first problem
 // function max(arrayOfNumbers){
 //     //google how to get largest number in array
 //     return Math.max(...arrayOfNumbers);
 // } // ... takes away array bracket const arr1 = [1,2,3]; --> console.log(Math.max(...arr1))
+
+// PLEASE NOTE: .sort() works well for strings, however numeric values, be carefull bc can return max as -62 even though 34 is the larger value
 
 // 2nd time
 // function max(arrayOfNumbers){
