2dd24ef28c43db7c0b2d8ea56d84d6e503aa4c82
Kamrun Foster <kamrun.foster4@gmail.com>
2021-07-12 10:48:42 -0500

    jquery selector  exercise

--- js/externaljQuery.js
+++ js/externaljQuery.js
@@ -24,19 +24,32 @@ $(document).ready(function(){
 
 
     // SELECTORS EXERCISE
-    // Id Selectors h1, p, ul, li
-    alert($("#cool").html());// alerts what is in id cool
-    alert($("#cool").html("This is no longer a cool paragraph"));// changes what's in id cool
-    $("p");
-    $("ul");
-    $("li");
 
+    // Id Selectors h1, p, ul, li
+    //1.- 4.
+    alert("Our first paragraph is: " + " " + $("#cool").html());// alerts the content of this element w/ id of cool
+    // alert($("#cool").html("This is no longer a cool paragraph"));// changes what's in id cool
+    //5. when we use same id on 2 elements, jquery applies change to first encounter with that id
+    alert("The first div contains: " + $("#something").html());
 
     // Class Selectors
+    // $(".codeup").css('border', '1px solid red');  //.css('border', '1px solid #F00');
+    // 5. giving another element the same class, will apply the same jquery to it
+    // 5. giving another class the id of codeup does NOT apply the class css border to this id
 
     // Element Selectors
+    //2.
+    // $("li").css('font-size','20px');
+    // //3.
+    // $("h1").css("background-color","yellow");
+    // $("p").css("background-color","green");;
+    // $("li").css("background-color","red");;
+    //
+    // alert($("h1").html()); //alerts our h1
+
 
     // Multiple Selectors
+    // $("h1, p, li").css("background-color","cyan");
 
 
 })
