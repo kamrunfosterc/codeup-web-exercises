78f519b137774dd4f6b8023da97a39f701e1dba5
Kamrun Foster <kamrun.foster4@gmail.com>
2021-07-12 14:28:46 -0500

    keyboard events exercise next

--- js/jQuery_Events_Lecture.js
+++ js/jQuery_Events_Lecture.js
@@ -5,23 +5,28 @@
 // $("h1").click("background-color","blue");// resource .css("background-color", "blue")
 
 //*************** *************** *************** *************** *************** ***************
-// JQuery mouse event exercise complete
-
+// JQuery mouse event EXERCISE
 $("h1").click(function (event){
     $(this).css("background-color", "orange");//will change background color when we click on h1
     $(this).css("color", "#fff")
 });
 
 $("p").dblclick(function (event){
-    $(this).css("font-size", "18px");//change fontsize to 18px when double clicked
+    $(this).css("font-size", "18px");//change fontsize to 18px when double clicked, goes per paragraph we click on
 });
 
 $("li").hover(function (event){
     // lets us know how many times we've hovered over a function both on and off
     //if passed w/ another function, this should only be hover in
-    $(this).css("color", "red")
+    $(this).css("color", "red");// hover in
 }, function (e){
     // hover out/away
-    $(this).css("color", "black")
+    $(this).css("color", "black"); // hover out
 });
+//*************** *************** *************** *************** *************** ***************
+
+
+//*************** *************** *************** *************** *************** ***************
+// JQuery Keyboard event EXERCISE
+
 //*************** *************** *************** *************** *************** ***************
